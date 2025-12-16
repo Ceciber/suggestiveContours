a00daac78549e2e78be6b0c70f0a7a6b24ef9355
Ceciber <cecilia_b2000@outlook.it>
2025-12-16 19:04:01 +0100

    Delete GeomTP directory

--- GeomTP/build/dep/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Proj/GeomTP/dep/glm
-# Build directory: D:/Proj/GeomTP/build/dep/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- GeomTP/build/dep/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Proj/GeomTP/dep/glm/glm
-# Build directory: D:/Proj/GeomTP/build/dep/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
