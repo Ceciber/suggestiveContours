d939eadcab51d355baee3bcbc5a3c52ea4712247
Ceciber <cecilia_b2000@outlook.it>
2025-12-16 19:04:40 +0100

    Delete build directory

--- build/dep/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/suggestive_contours/dep/glm
-# Build directory: D:/suggestive_contours/build/dep/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- build/dep/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/suggestive_contours/dep/glm/glm
-# Build directory: D:/suggestive_contours/build/dep/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
